d495dfe01f1a745ac49baf81913ec0a2ee92c172
alice19-meet <alice19@meet.mit.edu>
2018-08-05 16:29:54 +0300

    python

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,11 @@
-# Write your solution for 1.2 here!
+# Write your solution for 1.2 here!b=0
+b=0
+for i in range (101):
+    if i%2==0:
+        b=b+i
+print (b)
+i=0
+while i<1000:
+    if i%6==2 and i**3%5==3:
+        print(i)
+    i=i+1
